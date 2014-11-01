23ed6e914cf20e5a94a58f8844511192ef440544
akmorrow13 <akmorrow@wisc.edu>
2014-10-31 19:02:29 -0500

    some more comments

--- src/edu/wisc/cs/sdn/sr/Router.java
+++ src/edu/wisc/cs/sdn/sr/Router.java
@@ -422,8 +422,30 @@ public class Router
 				} 		
 
 			} else { // The gateway is different of zero when the packet needs to be forwarded to another router.
+				
+				// Check if the path is available. If the cost is greater than 16, than it is not
+				// a valid path. Send an error.
+				if(rtEntry.getCost() > 16)  {
+					// send ICMP message
+					byte[] payloadBytes = new byte[8];
+		
+					ByteBuffer bb = ByteBuffer.wrap(ipPacket.getPayload().serialize());
+		
+					bb.get(payloadBytes, 0, 8);
+		
+					IPv4 ipClone = (IPv4) ipPacket.clone();
+		
+					Data data = new Data();
+					data.setData(payloadBytes);
+		
+					ipClone.setPayload(data.deserialize(payloadBytes, 0, 8));
+					sendICMPMessage(ipPacket.getDestinationAddress(), ipPacket.getSourceAddress(), (byte) 0, (byte) 3, ipClone);
+					return;
+					
+				}				
 
 				entry = arpCache.lookup(rtEntry.getGatewayAddress());
+				
 
 				if (entry == null) {
 					// If the router does not know the destination MAC, call waitFor arp to enqueue the request
